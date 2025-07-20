6c07105d0607c435dc61efbe449ce7b98bbfb524
yourlocalcyborg <duwareon@gmail.com>
2025-07-20 17:10:33 -0400

    Added menu to startup to ask for mode

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def play_nim(player1, player2, total_stones=10):
         game.display()
         # Get current player
         current = player_list[game.current_player]
-        print(f"Player {current.name}'s turn")
+        print(f"\nPlayer {current.name}'s turn")
         # Select move based on player algorithm
         move = current.pick_move(game)
         # Apply move to the game
@@ -23,6 +23,55 @@ def play_nim(player1, player2, total_stones=10):
             print(f"Player {current.name} wins!")
         # Switch current player
         game.switch_player()
-p1 = players.HumanPlayer()
-p2 = players.MinimaxAI()
-play_nim(p1, p2)
+
+def pick_ai():
+    choice = input("(1) Random\n(2) Minimax\n(3) Alpha Beta\n(4) Expectiminimax\n> ")
+    if not choice.isdigit():
+        # Show input menu if incorrect input given
+        print("Please enter a given integer")
+        return pick_ai()
+    else:
+        if int(choice) == 1:
+            return players.RandomAI()
+        if int(choice) == 2:
+            return players.MinimaxAI()
+        if int(choice) == 3:
+            return players.AlphaBetaAI()
+        if int(choice) == 4:
+            return players.ExpectiminimaxAI()
+        else:
+            print("Please enter a given integer")
+            return pick_ai()
+
+def compare_ai():
+    print("TODO\n")
+        
+def get_players():
+    gamemode = input("Choose a game mode:\n(1) Player vs AI\n(2) AI vs AI\n(3) Compare AIs\n> ")
+    if not gamemode.isdigit():
+        print("Please enter a given integer")
+        return get_players()
+    else:
+        # Player vs AI
+        if int(gamemode) == 1:
+            p1 = players.HumanPlayer()
+            print("Please choose an AI for player 2")
+            p2 = pick_ai()
+        # AI vs AI
+        elif int(gamemode) == 2:
+            print("Please choose an AI for player 1")
+            p1 = pick_ai()
+            print("Please choose an AI for player 2")
+            p2 = pick_ai()
+        # Compare AI
+        elif int(gamemode) == 3:
+            compare_ai()
+            p1, p2 = None, None
+        else:
+            print("Please enter a given integer")
+            return get_players()
+        return p1, p2
+        
+p1, p2 = get_players()
+if not (p1 == None or p2 == None):
+    play_nim(p1, p2)
